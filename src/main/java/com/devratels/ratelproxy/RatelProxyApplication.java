95bd40a82c8b9bc1434f8ceff47d8d685a8c8ed2
dev-hobeoung <hobeoung.billionaire@gmail.com>
2022-08-02 23:04:30 +0900

    first commit by hobeoung2

---
+++ src/main/java/com/devratels/ratelproxy/RatelProxyApplication.java
@@ -0,0 +1,13 @@
+package com.devratels.ratelproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatelProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devratels/ratelproxy/RatelProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devratels.ratelproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
